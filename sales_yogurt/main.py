108b87e1ad6106ae5d41dfa59a5ee1ca2d1e9f25
Pascal Schoch <pasc.schoch@gmail.com>
2021-12-23 16:22:48 +0100

    commit change sales
    commit change sales

---
+++ sales_yogurt/main.py
@@ -0,0 +1,10 @@
+
+from auxillary.models import linear_model_, linear_model_lasso, random_forest_regressor
+from auxillary.train_test_split import train_test_split_
+from auxillary.error_functions import MAPE, RMSE
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    print("hi")
